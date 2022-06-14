018834cb5946ae409f909e3ca6a473478b371d52
Tina <tina.zhou.hui@gmail.com>
2022-06-14 22:10:47 +0100

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import dotenv from 'dotenv';
+import express from 'express';
+
+import router from './router';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // json body parser
+app.use(router); // apply router
+
+app.listen(PORT, () => {  // initialize server
+	console.log(`I am up and running at http://localhost:${PORT}`);
+});
