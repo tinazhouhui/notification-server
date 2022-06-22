103458d4307c49f378873dec796cb824827b5d08
Tina <tina.zhou.hui@gmail.com>
2022-06-22 13:20:20 +0100

    feat: get notifications

--- app/controllers/getNotifications.ts
+++ app/controllers/getNotifications.ts
@@ -1,9 +1,38 @@
-// import data from '../models/data';
-
 import {Request, Response} from 'express';
+import {prismaClient} from '../db/client';
 
-export function getNotifications (req: Request, res: Response) {
+export async function getNotifications(req: Request, res: Response) {
 	const {id} = req.params;
-	// console.log(data);
-	res.send({msg: id});
+	const likes = await prismaClient.like.findMany({
+		where: {
+			postId: id,
+			read: false
+		},
+		select: {
+			id: true,
+			user: true
+		}
+	});
+
+	const comments = await prismaClient.comment.findMany({
+		where: {
+			postId: id,
+			read: false
+		},
+		select: {
+			id: true,
+			text: true,
+			user: true
+		}
+	});
+
+	const output = {
+		notifications: {
+			likes: likes.length,
+			comments: comments.length,
+			total: likes.length + comments.length
+		}, likes, comments
+	};
+
+	res.send(output);
 }
